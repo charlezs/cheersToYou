127544c2318510258b8999e71ab3576d0e983ba7
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-09-24 22:39:14 -0400

    nested routes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,7 +4,7 @@ import { Layout } from '~/components/layout'
 import { UserPanel } from '~/components/user-panel'
 import { getOtherUsers } from '~/utils/user.server'
 import {json} from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { useLoaderData, Outlet } from '@remix-run/react'
 
 export const loader: LoaderFunction = async ({ request }) => {
     const userId = await requireUserId(request)
@@ -16,6 +16,7 @@ export default function Home() {
     const { users } = useLoaderData()
     return (
       <Layout>
+        <Outlet />
         <div className="h-full flex">
           <UserPanel users={users} />
           <div className="flex-1"></div>
--- app/routes/home/cheer.$userId.tsx
+++ app/routes/home/cheer.$userId.tsx
@@ -1,15 +1,13 @@
 import type { LoaderFunction } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 import { json } from '@remix-run/node'
-// 1
+
 export const loader: LoaderFunction = async ({ request, params }) => {
-  // 2
   const { userId } = params
   return json({ userId })
 }
 
 export default function KudoModal() {
-  // 3
   const data = useLoaderData()
   return <h2> User: {data.userId} </h2>
 }
